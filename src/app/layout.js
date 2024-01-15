ae95a57ea1a4f618df15faa40e1811cbbcb55961
WhyouDev <whyoudev@gmail.com>
2024-01-15 11:32:49 +0700

    change tructure organize

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 // import Navbar from '@/app/components/Utilities/Navbar'
-import Navbar from '@/app/components/Utilities/Navbar'
+import Navbar from '@/components/Utilities/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <Navbar/>
         {children}
         </body>

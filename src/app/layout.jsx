6e3e88dbd029c03ba3a03f697c0f84ab1c48da30
whyoudev <whyoudev@gmail.com>
2024-02-28 11:43:28 +0700

    remove trail space

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,9 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Gabarito } from 'next/font/google'
- 
-const gabarito = Gabarito({ 
-  subsets: ['latin'], 
+
+const gabarito = Gabarito({
+  subsets: ['latin'],
   display: 'swap',
   adjustFontFallback: false
 })
